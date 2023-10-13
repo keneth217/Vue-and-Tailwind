78b1c0f92795440449d0c4f02ae102c0fbac3aef
keneth217 <kipyegonkeneth03@gmail.com>
2023-10-13 16:36:30 +0300

    about page

---
+++ src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1 class="text-purple-600">This is an about page</h1>
+    <button class="text-white hover:text-red-500 ...">
+      Hover me
+    </button>
+  </div>
+</template>
+
+<style></style>
